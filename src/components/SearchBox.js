390a53b823960c7f53bc3543b019adc1505d2a59
Michael Clautice <michaelclauticeusa@gmail.com>
2022-06-01 15:37:22 -0400

    git push W.6.1.22

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -7,7 +7,7 @@ const SearchBox = ({ searchfield, searchChange }) => {
       <input
         className="pa3 ba b- -green bg-lightest-blue"
         type="search"
-        placeholder="search for robots!!!"
+        placeholder="search robots here"
         onChange={searchChange}
       />
     </div>
