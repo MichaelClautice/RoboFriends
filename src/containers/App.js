2c651064a70d5c81635a74d7bb9b6e9f0f740c1a
Michael Clautice <michaelclauticeusa@gmail.com>
2022-06-08 14:25:39 -0400

    W.6.8.22-2:25P

--- src/containers/App.js
+++ src/containers/App.js
@@ -45,7 +45,7 @@ class App extends Component {
       <h1>Loading</h1>
     ) : (
       <div className="tc">
-        <h1 className="f1">Mike's Robo Friends</h1>
+        <h1 className="f1">Mike's Robo Friends!</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <Scroll>
           <ErrorBoundary>
