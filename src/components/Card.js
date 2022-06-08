53b1f9e67c64a112ae840aa602bb3f76bd3c31f0
Michael Clautice <michaelclauticeusa@gmail.com>
2022-06-08 17:13:30 -0400

    W.6.8.22-5:13p

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ import React from "react";
 // name, email & id props are recieved as parameters & destructured at th same time
 const Card = ({ name, email, id }) => {
   return (
-    <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+    <div className="tc bg-moon-gray dib br3 pa3 ma2 grow bw2 shadow-5">
       <img alt="robots" src={`https://robohash.org/${id}?200x200`} />
       <div>
         <h2>{name}</h2>
